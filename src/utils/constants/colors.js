fd252ae826e87c9e395f01b253a112fe9957d667
Amar Nath <aamarnathkp@gmail.com>
2024-02-05 20:43:55 +0530

    feat: addeding new logo component

---
+++ src/utils/constants/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    green: "#01bf71",
+    red: "#e44f4f",
+    yellow: "#e6b800",
+};
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,31 @@
+import { colors } from "./constants/colors";
+
 export const calculateExperience = () => {
     const exp = Date.now() - new Date("04/01/2017");
     const expDate = new Date(exp);
 
     return `${Math.abs(expDate.getUTCFullYear() - 1970)}+ years`;
+};
+
+export const getThemeColor = (themeColor) => {
+    return Object.keys(colors).find((key) => colors[key] === themeColor);
+};
+
+export const getColorBasedLogoSrc = (themeColor) => {
+    const getColor = getThemeColor(themeColor);
+    console.log("getColor", getColor);
+    let logoSrc;
+    switch (getColor) {
+        case "green":
+            logoSrc = "/src/images/logo/logo_green.png";
+            break;
+        case "red":
+            logoSrc = "/src/images/logo/logo_red.png";
+            break;
+        default:
+            logoSrc = "/src/images/logo/logo_yellow.png";
+            break;
+    }
+    console.log("====SRC", logoSrc);
+    return logoSrc;
 };
