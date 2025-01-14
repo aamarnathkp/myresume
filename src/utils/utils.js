578db154103f4f3c175faa4657537a608c243e4d
Amar Nath <aamarnathkp@gmail.com>
2025-01-14 13:35:04 +0530

    fix: changing exp year to string

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,7 @@ export const calculateExperience = () => {
     const exp = Date.now() - new Date("04/01/2017");
     const expDate = new Date(exp);
 
-    return `${Math.abs(expDate.getUTCFullYear() - 1970)}+ years`;
+    return Math.abs(expDate.getUTCFullYear() - 1970);
 };
 
 export const getThemeColor = (themeColor) => {
